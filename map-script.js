a1faff6ef1359815e7bc0d7cc5434cb636f50712
Allain <allaindemoupu@mail.ru>
2023-03-03 08:10:23 +0300

    Увеличиние карт при нажатии

--- map-script.js
+++ map-script.js
@@ -1,12 +1,27 @@
 console.log("Скрипт странички с картами успешно загружен")
 
 const img = document.getElementById("map1");
+let imgSize = 1;
 const igroki = document.getElementById("igroki");
 const polit = document.getElementById("polit");
 const resources = document.getElementById("resources");
 const ethic = document.getElementById("ethic");
 const religion = document.getElementById("religion");
 const climate = document.getElementById("climate");
+
+img.addEventListener("click",() => {
+    if (imgSize == 1) {
+        img.style.width = "200%"
+        img.style.height = "200%"
+        imgSize += 1
+    } else {
+        img.style.width = "100%"
+        img.style.height = "100%"
+        imgSize -= 1
+
+    }
+    
+});
 
 igroki.addEventListener("click",() => {
     img.src="maps/igroki.png"
